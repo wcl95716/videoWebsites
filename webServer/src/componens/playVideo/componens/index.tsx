98aa3c5325f152c3b8944848c344af8ac7bd461c
chenglongwen <chenglongwen@microsoft.com>
2023-04-21 11:41:08 +0800

    update

--- webServer/src/componens/playVideo/componens/index.tsx
+++ webServer/src/componens/playVideo/componens/index.tsx
@@ -1,16 +1,119 @@
 import React, { useState, useEffect } from 'react';
 import { VideoDetail } from '../index.model';
 import "./index.css"
-const VideoPreviewImg : React.FC<{videoDetail :VideoDetail, videoUrl:string}> = ({videoDetail, videoUrl}) => {
-  const url = `${videoUrl}toVideoPreview?video_name=${videoDetail.name}`
+
+export const VideoPreviewItem: React.FC<{ videoDetail: VideoDetail, videoUrl: string }> = ({ videoDetail, videoUrl }) => {
+
+  // 只有前端点击一下的时候才播放 
+  // 播放的时候点击第二下可以触发外部包裹的a标签跳转
+  const [isPlay, setIsPlay] = useState(false);
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+
+
+  // 点击VideoPreviewItem 不触发a标签跳转
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.addEventListener('click', (event) => {
+        if (!isPlay) {
+          event.stopPropagation();
+        }
+
+      });
+    }
+  }, []);
+  useEffect(() => {
+    if (isPlay) {
+      videoRef.current?.play();
+    } else {
+      videoRef.current?.pause();
+    }
+  }, [isPlay]);
+
+  // 鼠标在video上面的时候播放视频
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.addEventListener('mouseenter', (event) => {
+        event.stopPropagation();
+        setIsPlay(true);
+      });
+      video.addEventListener('mouseleave', () => {
+        setIsPlay(false);
+      });
+    }
+  }, []);
+  // 手机端也支持
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.addEventListener('touchstart', (event) => {
+        event.stopPropagation();
+        setIsPlay(true);
+
+      });
+      video.addEventListener('touchend', () => {
+        //setIsPlay(false);
+      });
+    }
+  }, []);
+
+  const previewUrl = `${videoUrl}getVideoPreview?video_name=${videoDetail.name}`;
+  // 如果previewUrl 没有响应 三秒后再次访问
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     const video = videoRef.current;
+  //     if (video) {
+  //       video.src = previewUrl;
+  //     }
+  //   }, 3000);
+  //   return () => {
+  //     clearTimeout(timer);
+  //   };
+  // }, [previewUrl]);
+
+
   return (
-    <div>
-        <img className="image-container" src={url} 
-         alt="Video Preview" 
-        />
-    </div>
-    
+    <>
+      <div style={{
+        // 居中
+        position: "relative",
+        height: "auto",
+      }}
+        aria-disabled
+        className="video-preview-item" onClick={() => setIsPlay(!isPlay)}
+      >
+          <video
+            ref={videoRef}
+            className="video-preview-item-video"
+            src={`${videoUrl}getVideoPreview?video_name=${videoDetail.name.replace('&', '%26').replace('%26', '%2526')}`}
+            //controls
+            autoPlay
+            muted
+            loop
+            width="320" height="240"
+            playsInline
+          />
+      </div>
+
+    </>
   );
 };
 
-export default VideoPreviewImg;
+//   return (
+//     <>
+//       <div className="video-preview-item">
+//         <video
+//           className="video-preview-item-video"
+//           src={`${videoUrl}getVideoPreview?video_name=${videoDetail.name}`}
+//           controls
+//           autoPlay
+//           muted
+//           loop
+//           width="320" height="240"
+//         />
+//         <div className="video-preview-item-title">{videoDetail.name}</div>
+//       </div>
+//     </>
+//   );
+// };
--- webServer/src/componens/playVideo/test.tsx
+++ webServer/src/componens/playVideo/test.tsx
@@ -1,60 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { VideoDetail, getVideoDetailList, selectVideoDetailList } from './index.model';
 import React, { useEffect, useRef, useState } from 'react';
-import VideoPreviewImg from './componens';
+import { Pagination } from 'antd';
+import { VideoPreviewItem } from './componens';
 
-
-const VideoPreview: React.FC<{videoDetail :VideoDetail, videoUrl:string}> = ({videoDetail, videoUrl}) => {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-  const [currentSegment, setCurrentSegment] = useState(0);
-
-  const segments = [
-    { start: 10, end: 13 },
-    { start: 30, end: 33 },
-    { start: 50, end: 53 },
-  ];
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current?.play();
-      videoRef.current.load();
-      videoRef.current.play();
-    }
-  }, [currentSegment]);
-
-  const onTimeUpdate = () => {
-    const video = videoRef.current;
-    if (video && video.currentTime >= segments[currentSegment].end) {
-      if (currentSegment < segments.length - 1) {
-        setCurrentSegment(currentSegment + 1);
-        videoRef.current?.play();
-      } else {
-        video.pause();
-      }
-    }
-  };
-  const videoURL = `${videoUrl}getVideo?video_name=${videoDetail.name}`;
-  const previewURL = `${videoURL}#t=${segments[currentSegment].start},${segments[currentSegment].end}`;
-
-
-  return (
-    <div>
-      {videoURL && (
-        <video
-          ref={videoRef}
-          width="320"
-          height="240"
-          controls
-          autoPlay={true}
-          preload="metadata"
-          onTimeUpdate={onTimeUpdate}
-        >
-          <source src={previewURL} type="video/mp4" />
-          您的浏览器不支持 video 标签。
-        </video>
-      )}
-    </div>
-  );
-}
 
 // 展示播放视频列表,随机选取一帧画面做预览图
 // 点击按钮跳转播放视频
@@ -64,22 +13,126 @@ export const VideoList: React.FC = () => {
 
   React.useEffect(() => {
     dispatch(getVideoDetailList() as any);
+    
   }, [dispatch]);
+
+  // 添加翻页功能
+  const pageStorage = localStorage.getItem('page');
+  const pageSizeStorage = localStorage.getItem('pageSize');
+
+  const [page, setPage] = useState(pageStorage ? parseInt(pageStorage) : 1);
+  const [pageSize, setPageSize] = useState(pageSizeStorage ? parseInt(pageSizeStorage) : 10);
+  const [total, setTotal] = useState(0);
+  const [videoList, setVideoList] = useState<VideoDetail[]>([]);
+
+  const videoListRef = useRef(videoList);
+
+  useEffect(() => {
+    videoListRef.current = videoList;
+  }, [videoList]);
+
+  useEffect(() => {
+    const videoList = videoDetailList.videos.slice((page - 1) * pageSize, page * pageSize);
+    setVideoList(videoList);
+    setTotal(videoDetailList.videos.length);
+  }, [page, pageSize, videoDetailList.videos]);
+
+  const pageChange = (page: number, pageSize?: number) => {
+    setPage(page);
+    setPageSize(pageSize || 10);
+  };
+
+  const pagination = {
+    current: page,
+    pageSize: pageSize,
+    total: total,
+    onChange: pageChange,
+    showSizeChanger: true,
+    onShowSizeChange: (current:number , size:number) => {
+      setPage(current);
+      setPageSize(size);
+    },
+  };
+
+  // 刷新页面的时候保留当前页
+  useEffect(() => {
+    const page = localStorage.getItem('page');
+    const pageSize = localStorage.getItem('pageSize');
+    if (page && pageSize) {
+      setPage(parseInt(page));
+      setPageSize(parseInt(pageSize));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('page', page.toString());
+    localStorage.setItem('pageSize', pageSize.toString());
+  }, [page, pageSize]);
+
 
   return (
     <>
       <div>
         <h1>视频播放列表</h1>
+        <Pagination {...pagination} />
         <div>
-          {videoDetailList.videos.map((videoDetail) => (
-            <div>
-              {/* <a href={`${videoDetailList.videoUrl}toVideo?video_name=${videoDetail.name}`}>{videoDetail.name}</a> */}
-              {/* <VideoPreview videoDetail={videoDetail} videoUrl={videoDetailList.videoUrl}/> */}
-              <VideoPreviewImg videoDetail={videoDetail} videoUrl={videoDetailList.videoUrl}/>
+          {videoList.map((videoDetail) => (
+            <div style={{  height: "auto", }}>
+              <a
+              // 点击打开新页面
+                target="_blank" 
+                // 文件名中包含&符号的时候会导致url解析错误
+                // 解决方法：将&符号转换为%26
+                // 但是这个方法有缺陷，如果文件名中包含%26，那么就会被转义成&符号
+                // 所以这里需要先将%26转义成%2526
+                href={`${videoDetailList.videoUrl}toVideo?video_name=${videoDetail.name.replace('&', '%26').replace('%26', '%2526')}`}
+                // href={`${videoDetailList.videoUrl}toVideo?video_name=${videoDetail.name}`}
+                //href={`${videoDetailList.videoUrl}toVideo?video_name=${(videoDetail.name)}`}
+                >
+                <div  >
+                  <VideoPreviewItem videoDetail={videoDetail} videoUrl={videoDetailList.videoUrl} />
+                </div>
+                 {videoDetail.name} 
+              </a>
             </div>
           ))}
+        </div>
+        <div>
+          <br />
+          <br />
+          <br />
+        </div>
+        <Pagination {...pagination} />
+        <div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
         </div>
       </div>
     </>
   );
 };
+
+//   return (
+//     <>
+//       <div>
+//         <h1>视频播放列表</h1>
+//         <div>
+//           {videoDetailList.videos.map((videoDetail) => (
+//             <div>
+//               <a href={`${videoDetailList.videoUrl}toVideo?video_name=${videoDetail.name}`}>
+//                 <video width="320" height="240" controls>
+//                   <source src={`${videoDetailList.videoUrl}getVideoPreview?video_name=${videoDetail.name}`} type="video/mp4" />
+//                 </video>
+//                 {videoDetail.name}
+//               </a>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
